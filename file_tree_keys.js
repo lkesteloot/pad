29e76840375a25a50ada37754f5b0d1df88bc62b
Lawrence Kesteloot <lk@teamten.com>
2014-02-19 00:25:22 -0800

    Can type filename in file tree to jump to the file.

--- file_tree_keys.js
+++ file_tree_keys.js
@@ -4,11 +4,17 @@
 
 var util = require("util");
 var AbstractKeys = require("./abstract_keys");
+var strings = require("./strings");
 
 var FileTreeKeys = function () {
     AbstractKeys.call(this);
+    this.search = "";
 };
 util.inherits(FileTreeKeys, AbstractKeys);
+
+FileTreeKeys.prototype.getState = function () {
+    return this.search;
+};
 
 FileTreeKeys.prototype.onKey = function (key, pane, callback) {
     switch (key) {
@@ -36,9 +42,51 @@ FileTreeKeys.prototype.onKey = function (key, pane, callback) {
         case ";":
             pane.window.activateCommandPane();
             break;
+
+        case "\x08": // Backspace
+        case "\x7F": // Delete
+            if (this.search.length > 0) {
+                this.search = this.search.substring(0, this.search.length - 1);
+                this.updateSearch(pane);
+            }
+            break;
+
+        default:
+            if (this.isFilenameKey(key)) {
+                this.search += key;
+                this.updateSearch(pane);
+            }
+            break;
     }
 
     process.nextTick(callback);
+};
+
+FileTreeKeys.prototype.isFilenameKey = function (key) {
+    return key >= " " && key < "\x7F" && key != "/" && key != "\\" &&
+        key != ":" && key != ";" && key != "*" && key != "?";
+};
+
+FileTreeKeys.prototype.updateSearch = function (pane) {
+    // Update status line.
+    pane.redrawDirty = true;
+
+    // Find the line that starts with the search prefix.
+    var docIndex = 0;
+    while (true) {
+        var nextDocIndex = pane.doc.findNextLine(docIndex);
+        if (nextDocIndex === docIndex) {
+            break;
+        }
+
+        var text = pane.doc.toString(docIndex, nextDocIndex);
+        if (strings.startsWith(text, this.search)) {
+            pane.desiredDocIndex = docIndex;
+            break;
+        }
+
+        docIndex = nextDocIndex;
+    }
 };
 
 module.exports = FileTreeKeys;
